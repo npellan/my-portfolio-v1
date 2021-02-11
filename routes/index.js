083fd1a818dd4bb9bb058a2e3004ba693d727386
npellan <npellan@gmail.com>
2021-02-11 18:27:57 +0100

    Création des autres pages et des routes !

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,16 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Nicolas Pellan - Développeur web' });
 });
 
+router.get('/projets', (req, res, next) => {
+  res.render('projets', { title: 'Projets | Nicolas Pellan - Développeur web' });
+});
+
+router.get('/a-propos', (req, res, next) => {
+  res.render('apropos', { title: 'A propos | Nicolas Pellan - Développeur web' });
+});
+
+router.get('/contact', (req, res, next) => {
+  res.render('contact', { title: 'A propos | Nicolas Pellan - Développeur web' });
+});
+
 module.exports = router;
