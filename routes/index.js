3570730dbd891557156fe399686e50219e920678
npellan <npellan@gmail.com>
2021-02-12 16:44:45 +0100

    Ajout pages par projet, avec JSON et routes param

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const projets = require('../source/data/projets.json');
 
 const router = express.Router();
 
@@ -9,6 +10,15 @@ router.get('/', (req, res, next) => {
 
 router.get('/projets', (req, res, next) => {
   res.render('projets', { title: 'Projets | Nicolas Pellan - Développeur web' });
+});
+
+router.get('/projets/:id', (req, res, next) => {
+  const projetId = parseInt(req.params.id, 10);
+
+  const projet = projets.find(
+    (elem) => elem.id === projetId,
+  );
+  res.render('projet', { title: 'Projets | Nicolas Pellan - Développeur web', projet });
 });
 
 router.get('/a-propos', (req, res, next) => {
