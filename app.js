17eeae3609affab743fa776b5f817a807c9c7119
npellan <npellan@gmail.com>
2021-02-19 11:41:26 +0100

    removed helmet and compression

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(compression());
-app.use(helmet());
+// app.use(compression());
+// app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
