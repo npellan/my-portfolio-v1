605d6b048d0af30f44d50e114ae96957a3832c6b
Nicolas <npellan@gmail.com>
2021-04-06 15:01:57 +0200

    🎨  refactored app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,36 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toTop = document.querySelector('.to-top');
+const app = {
+  toTop: document.querySelector('.to-top'),
+  darkModeSwitch: document.querySelector('.switch__input'),
 
-  function scrollFunction() {
+  init: () => {
+    app.toTop.addEventListener('click', app.topFunction);
+    app.darkModeSwitch.addEventListener('change', app.toggleDarkMode);
+    window.onscroll = () => { app.scrollFunction(); };
+  },
+
+  scrollFunction: () => {
     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-      toTop.classList.add('show');
+      app.toTop.classList.add('show');
     } else {
-      toTop.classList.remove('show');
+      app.toTop.classList.remove('show');
     }
-  }
+  },
 
-  function topFunction() {
+  topFunction: () => {
     window.scrollTo({
       top: 0,
       left: 0,
       behavior: 'smooth',
     });
-  }
+  },
 
-  window.onscroll = () => { scrollFunction(); };
-
-  toTop.addEventListener('click', topFunction);
-
-  function toggleDarkMode() {
+  toggleDarkMode: () => {
     const { body } = document;
     body.classList.toggle('dark-mode');
     document.querySelector('.np-logo-st0').classList.toggle('np-logo-dark-mode');
     const paths1 = document.querySelectorAll('.np-logo-st1');
     paths1.forEach((path) => path.classList.toggle('np-logo-dark-mode'));
-  }
+  },
+};
 
-  const darkModeSwitch = document.querySelector('.switch__input');
-
-  darkModeSwitch.addEventListener('change', toggleDarkMode);
-});
+document.addEventListener('DOMContentLoaded', app.init());
