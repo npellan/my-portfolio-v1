f03c9213521ba2400dce8ba4a056c352e70d03c5
Nicolas <npellan@gmail.com>
2021-04-06 16:39:24 +0200

    💥  dark mode persistent in localStorage

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,16 @@
 const app = {
   toTop: document.querySelector('.to-top'),
   darkModeSwitch: document.querySelector('.switch__input'),
+  isDarkModeOn: null,
 
   init: () => {
+    // button to scroll top
     app.toTop.addEventListener('click', app.topFunction);
-    app.darkModeSwitch.addEventListener('change', app.toggleDarkMode);
     window.onscroll = () => { app.scrollFunction(); };
+    // toggle dark mode
+    app.darkModeSwitch.addEventListener('change', app.toggleDarkMode);
+    app.isDarkModeOn = localStorage.getItem('isDarkModeOn');
+    app.setTheme();
   },
 
   scrollFunction: () => {
@@ -25,11 +30,29 @@ const app = {
   },
 
   toggleDarkMode: () => {
+    // toggling classes
     const { body } = document;
     body.classList.toggle('dark-mode');
     document.querySelector('.np-logo-st0').classList.toggle('np-logo-dark-mode');
     const paths1 = document.querySelectorAll('.np-logo-st1');
     paths1.forEach((path) => path.classList.toggle('np-logo-dark-mode'));
+    // localStorage
+    let isDarkModeOn = 'false';
+    if (document.body.classList.contains('dark-mode')) {
+      isDarkModeOn = 'true';
+    }
+    localStorage.setItem('isDarkModeOn', isDarkModeOn);
+  },
+
+  setTheme: () => {
+    if (app.isDarkModeOn === 'true') {
+      const { body } = document;
+      body.classList.add('dark-mode');
+      document.querySelector('.np-logo-st0').classList.add('np-logo-dark-mode');
+      const paths1 = document.querySelectorAll('.np-logo-st1');
+      paths1.forEach((path) => path.classList.add('np-logo-dark-mode'));
+      document.querySelector('.switch__input').checked = true;
+    }
   },
 };
 
