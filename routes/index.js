55f9d846e0a24ba3434b2a6e877bc114e97cc0a2
Nicolas <npellan@gmail.com>
2022-09-19 14:24:47 +0200

    :speech_balloon: updates content

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,8 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   res.locals = {
     ...res.locals,
-    title: 'Nicolas Pellan - Développeur web fullstack JavaScript, React, Node.js',
-    description: 'Bonjour, je suis développeur web fullstack JavaScript, spécialisé en React, Vue.js et Node.js, formé à l\'école O\'Clock.',
+    title: 'Nicolas Pellan - Développeur web fullstack JavaScript, Vue.js, React, Node.js',
+    description: 'Bonjour, je suis développeur web fullstack JavaScript, spécialisé en Vue.js, React et Node.js, formé à l\'école O\'Clock.',
     url: '/',
   };
   res.render('index', {
@@ -23,7 +23,7 @@ router.get('/projets', (req, res, next) => {
     ...res.locals,
     projets,
     title: 'Projets | Nicolas Pellan - Développeur web',
-    description: 'Découvrez tous les projets que j\'ai réalisé en tant que développeur web fullstack JavaScript, React, Vue.js et Node.js',
+    description: 'Découvrez tous les projets que j\'ai réalisé en tant que développeur web fullstack JavaScript, Vue.js, React et Node.js',
     url: '/projets',
   };
   res.render('projets', {
